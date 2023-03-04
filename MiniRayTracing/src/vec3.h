f80d7475c8ab06215e33b999daa2c8c1fe6e1c20
berkealgul <berkealgul@gmail.com>
2023-03-04 19:45:20 +0300

    vec3 done

--- MiniRayTracing/src/vec3.h
+++ MiniRayTracing/src/vec3.h
@@ -12,9 +12,9 @@ public:
 
 	Vec3& operator+=(Vec3& v)
 	{
-		e[0] += v.x();
-		e[1] += v.y();
-		e[2] += v.z();
+		e[0] += v[0];
+		e[1] += v[1];
+		e[2] += v[2];
 		return *this;
 	}
 
@@ -30,7 +30,7 @@ public:
 		return *this *= 1 / t;
 	}
 
-	double length () const
+	double length() const
 	{
 		return std::sqrt(length_squared());
 	}
@@ -40,22 +40,74 @@ public:
 		return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
 	}
 
-	inline double operator[](int i) { return e[i]; }
+	inline Vec3 unit()
+	{
+		return *this / length();
+	}
 
-	inline double x() { return e[0]; }
-	inline double y() { return e[1]; }
-	inline double z() { return e[2]; }
+	inline double operator[](int i) const { return e[i]; }
 
-	//inline std::ostream& operator<<(std::ostream& out)
-	//{
-	//	return out << e[0] << ' ' << e[1] << ' ' << e[2];
-	//}
+	inline double x() const { return e[0]; }
+	inline double y() const { return e[1]; }
+	inline double z() const { return e[2]; }
 
 private:
 	double e[3];
 
 };
 
+inline std::ostream& operator<<(std::ostream& out, const Vec3& v)
+{
+	return out << v[0] << ' ' << v[1] << ' ' << v[2];
+}
+
+inline Vec3 operator+(const Vec3& u, const Vec3& v)
+{
+	return Vec3(u[0] + v[0], u[1] + v[1], u[2] + v[2]);
+}
+
+inline Vec3 operator-(Vec3& u, Vec3& v)
+{
+	return Vec3(u[0] - v[0], u[1] - v[1], u[2] - v[2]);
+}
+
+inline Vec3 operator*(Vec3& u, Vec3& v)
+{
+	return Vec3(u[0] * v[0], u[1] * v[1], u[2] * v[2]);
+}
+
+inline Vec3 operator*(double t, Vec3& v)
+{
+	return Vec3(t * v[0], t * v[1], t * v[2]);
+}
+
+inline Vec3 operator*(Vec3& v, double t)
+{
+	return t * v;
+}
+
+inline Vec3 operator/(Vec3& v, double t)
+{
+	return (1 / t) * v;
+}
+
+inline double dot(const Vec3& u, const Vec3& v) 
+{
+	return u[0] * v[0] + u[1] * v[1] + u[2] * v[2];
+}
+
+inline Vec3 cross(const Vec3& u, const Vec3& v) 
+{
+	return Vec3(
+		u[1] * v[2] - u[2] * v[1],
+		u[2] * v[0] - u[0] * v[2],
+		u[0] * v[1] - u[1] * v[0]);
+}
+
+inline Vec3 unit_vector(Vec3 v) 
+{
+	return v / v.length();
+}
 
 using Point3 = Vec3;
 using Color = Vec3;
