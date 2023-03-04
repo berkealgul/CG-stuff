17fa7d67c4af3c653097f51157581b78878771b6
berkealgul <berkealgul@gmail.com>
2023-03-04 21:05:17 +0300

    color write

--- MiniRayTracing/src/ray.h
+++ MiniRayTracing/src/ray.h
@@ -6,12 +6,22 @@
 class Ray
 {
 public:
-	Ray() {}
+	Ray(const Point3& origin, const Vec3& direction) :
+		orig(origin),
+		dir(direction)
+	{}
 
+	Point3 origin() const { return orig; }
+	Vec3 direction() const { return dir; }
+
+	Point3 at(double t)
+	{
+		return orig + t * dir;
+	}
 
 private:
-	Vec3 origin;
-	Vec3 direction;
+	Vec3 orig;
+	Vec3 dir;
 };
 
 
