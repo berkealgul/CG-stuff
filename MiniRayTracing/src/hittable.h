0c9a78a2c7e0b10df0b7a2b12051b4304ddee6be
berkealgul <berkealgul@gmail.com>
2023-04-24 23:08:46 +0300

    hittable face normal

--- MiniRayTracing/src/hittable.h
+++ MiniRayTracing/src/hittable.h
@@ -9,12 +9,21 @@ struct HitResult
     Point3 p;
     Vec3 normal;
     double t;
+    bool front_face;
+
+    inline void set_face_normal(const ray& r, const vec3& outward_normal) 
+    {
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
 };
 
 class Hittable 
 {
 public:
     virtual bool hit(const Ray& r, double t_min, double t_max, HitResult& rec) const = 0;
+
+
 
 };
 
